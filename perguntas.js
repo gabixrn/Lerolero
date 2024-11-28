90a902a61b43c9ab9f2c9ac663b13935f242894d
gabixrn <00001241041787sp@al.educacao.sp.gov.br>
2024-11-28 15:54:10 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Esporte',
+    'Quem inventou o basquete?',
+    'James Naismith'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior deserto do Brasil?',
+    'Deserto do Jalapão'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual é o animal mais forte do mundo?',
+    'Elefante Africano'
+)
+
+criaCartao(
+    'Geografia',
+    'Quantos fusos horários existem no Brasil?',
+    'Quatro'
+)
